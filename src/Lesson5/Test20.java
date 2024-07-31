c0db86e775fcfdedbe0d63c7368f7a7119affbdf
Infix <rxyz@gmail.com>
2024-07-31 19:45:58 +0300

    Lesson 5 continue

---
+++ src/Lesson5/Test20.java
@@ -0,0 +1,18 @@
+package Lesson5;
+
+public class Test20 {
+    int age;
+
+    Test20(){}
+
+    Test20(int age)
+    {
+        this.age = age;
+    }
+
+    public static void main(String[] args)
+    {
+        Test20 t = new Test20();
+        System.out.println(t.age);
+    }
+}
